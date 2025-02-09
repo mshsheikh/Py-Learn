9b42ef8cc2e4983469de4fc5c805bbbe985f1758
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-02-09 18:57:10 +0500

    Required Imports: random

---
+++ hangman.py
@@ -0,0 +1,141 @@
+import random
+
+def get_word():
+    """Fetches a random word from a predefined list."""
+    words = ["python", "hangman", "challenge", "programming", "developer", "algorithm", "function", "variable"]
+    return random.choice(words).upper()
+
+def display_hangman(tries):
+    """Displays the hangman ASCII art based on the number of tries left."""
+    stages = [
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |     / \\
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |     / 
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |      
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|
+           |      |
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |      |
+           |      |
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |    
+           |      
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      
+           |    
+           |      
+           |     
+           -
+        """
+    ]
+    return stages[tries]
+
+def play_game():
+    """Main function to play the Hangman game."""
+    word = get_word()
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Welcome to Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess.")
+        
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    
+    if guessed:
+        print("Congratulations! You guessed the word:", word)
+    else:
+        print("Sorry, you ran out of tries. The word was:", word)
+
+def main():
+    """Main entry point for the game."""
+    while True:
+        play_game()
+        replay = input("Do you want to play again? (Y/N): ").upper()
+        if replay != "Y":
+            print("Thanks for playing! Goodbye!")
+            break
+
+if __name__ == "__main__":
+    main()
