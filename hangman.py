f43e2481c1ab47dfb7be1afb1c4b838dfb59dae6
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-03-20 15:58:57 +0500

    Required Import: random

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ import random
 
 def get_word():
     """Fetches a random word from a predefined list."""
-    words = ["python", "developer", "algorithm", "function", "variable", "import", "lambda", "string", "object", "method", "module"]
+    words = ["python", "algorithm", "function", "variable", "import", "lambda", "string", "object", "method", "module"]
     return random.choice(words).upper()
 
 def display_hangman(tries):
